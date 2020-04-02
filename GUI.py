2802584c5f105391fd54ff3948f7adc6048cdbf1
Eugene Mironov <mir-evgenii@mail.ru>
2020-04-02 20:08:52 +0300

    Rework GUI.

--- GUI.py
+++ GUI.py
@@ -1,26 +1,38 @@
 from tkinter import *
+from Astar import Astar
 
 class GUI:
 
-    def __init__(self, maze, res):
+    def __init__(self, maze):
+
+        self.start_node = 0
+        self.end_node = 0
+
+        self.frame_list = []
 
         self.root = Tk()
 
-        maze = self.show_res_in_maze(maze, res)
+        self.maze = maze
+        #maze = self.show_res_in_maze(maze, res)
 
-        self.maze(maze)
+        self.maze_print(maze)
 
         self.root.mainloop()
 
-    def maze(self, maze):
+    def maze_print(self, maze):
 
+        b = 0
         for list in maze:
             frame = Frame(self.root)
+            self.frame_list.append(frame)
             for i in list:
-                but = Button(frame)
+                b = b + 1
+                but = Button(frame, command=lambda c=str(b): self.init_node(c), activebackground="red")
                 but["text"] = i
+                but.bind("<Button-1>")
                 but.pack(side=LEFT)
             frame.pack()
+
 
     def show_res_in_maze(self, maze, res):
 
@@ -29,6 +41,33 @@ class GUI:
 
         return maze
 
+    def init_node(self, i):
+        if (self.start_node == 0):
+            self.start_node = self.node_init_from_num(i)
+            print(self.start_node)
+        else:
+            self.end_node =  self.node_init_from_num(i)
+            print(self.end_node)
+            self.find_path()
 
+    def node_init_from_num(self, i):
+        if (int(i) > 10):
+            x = str(i)[0]
+            y = str(i)[1]
+            return (int(x), int(y)-1)
+        else:
+            return (0, int(i)-1)
+
+
+    def find_path(self):
+
+        path = Astar(self.maze, self.start_node, self.end_node)
+        res = path.find()
+        maze = self.show_res_in_maze(self.maze, res)
+
+        for frame in self.frame_list:
+            frame.destroy()
+
+        self.maze_print(maze)
 
 
--- main.py
+++ main.py
@@ -12,20 +12,16 @@ maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-start = (0, 0)
-end = (7, 6)
+#start = (0, 0)
+#end = (7, 6)
 
-path = Astar(maze, start, end)
-res = path.find()
-
-#print(res)
-
-gui = GUI(maze, res)
+#path = Astar(maze, start, end)
+#res = path.find()
 
 
-#for node in res:
-#    print('index = '+str(node[0]))
-#    print('item = '+str(node[1]))
+gui = GUI(maze)
+
+
 
 
 
